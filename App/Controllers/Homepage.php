c391c8cdba84a13b359c4f8870f599c002b6df9d
Robert Hill <rhill34@mail.greenriver.edu>
2019-04-17 19:48:12 -0700

    Created the prototype versioning branch
    echo todos from Controllers, preparing to reorganize the master branch. -RH3

--- App/Controllers/Homepage.php
+++ App/Controllers/Homepage.php
@@ -3,12 +3,12 @@
 class Homepage {
 
     public function index(){
-        echo"<h1> TODO Homepage extra stuff</h1>";
-//        $f3=\Base::instance();
-//        $data = array();
-//        $data['nav'] = $f3->alias('nav_home' , '/');
-//        $data['personal'] = $f3->alias('/person');
-//        $view = new Template();
-//        echo $view->render('views/home.php','text/html', $data);
+//        echo"<h1> TODO Homepage extra stuff</h1>";
+        $f3=\Base::instance();
+        $data = array();
+        $data['nav'] = $f3->alias('nav_home' , '/');
+        $data['personal'] = $f3->alias('/person');
+        $view = new Template();
+        echo $view->render('views/home.php','text/html', $data);
     }
 }
